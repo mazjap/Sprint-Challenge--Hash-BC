8502a14d95d363d6fe9bd2546be3993f9a63e5d2
Jordan Christensen <jordan.c4922@gmail.com>
2020-03-13 12:31:03 -0600

    Passing all tests but 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,31 +8,24 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
-    if len(weights) < 2:
-        return (None, None)
-
-    for key in weights:
-        value = limit - key
-        hash_table_insert(ht, key, value)
-
-    for index in range(len(weights)):
-        key = weights[index]
-        value = limit - key
-        if hash_table_retrieve(ht, value):
-            second_index = weights.index(hash_table_retrieve(ht, key))
-            return (max(index, second_index), min(index, second_index))
+    if len(weights) > 1:
+#       [12, 6, 7, 14, 19, 3, 0, 25, 40]
+        for index in range(len(weights)):
+            key = weights[index]
+            value = limit - key
+            if value >= 0:
+                hash_table_insert(ht, key, value)
+            
+            if hash_table_retrieve(ht, value):
+                second_index = weights.index(hash_table_retrieve(ht, key))
+                if index is not second_index:
+                    return (max(index, second_index), min(index, second_index))
 
 def print_answer(answer):
     if answer is not None:
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
-
-
-weights_2 = [4, 4]
-answer_2 = get_indices_of_item_weights(weights_2, 2, 8)
-print(f"answer_2[0] ({answer_2[0]}) == 1: {answer_2[0] == 1}")
-print(f"answer_2[1] ({answer_2[1]}) == 0: {answer_2[1] == 0}")
 
 
 weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
